1a66932b2992fb8b0f68d814befe1502c2f4e501
JavierSantaCruzXD <2010204javiergomez@gmail.com>
2021-01-07 09:03:55 -0600

    finish page without audio

--- app/mailers/contract_mailer.rb
+++ app/mailers/contract_mailer.rb
@@ -2,7 +2,6 @@ class ContractMailer < ApplicationMailer
 
     default from: "micorreo@gmail.com"
 
-
     def download_contract(contract)
         @contract = contract
         mail(to: @contract.email, bcc:'admin@utesis.com', subject: "Contrato Utesis")
